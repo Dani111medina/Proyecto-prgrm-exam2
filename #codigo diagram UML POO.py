0340e1290e5b22d71eee1970d5488a08745f3785
Dani111medina <159402129+Dani111medina@users.noreply.github.com>
2025-04-30 19:45:37 -0500

    Add files via upload

---
+++ #codigo diagram UML POO.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+class Persona:
+    def __init__(self, nombre: str, nif: str, fechaNac: date):
+        self.nombre = nombre
+        self.nif = nif
+        self.fechaNac = fechaNac
+
+    def __str__(self):
+        return f"{self.nombre}, NIF: {self.nif}, Fecha de Nac: {self.fechaNac}"
+
+class Jugador(Persona):
+    def __init__(self, nombre: str, nif: str, fechaNac: date, numFed: int):
+        super().__init__(nombre, nif, fechaNac)
+        self.numFed = numFed
+
+    def __str__(self):
+        return f"{super().__str__()}, Número Federación: {self.numFed}"
+
+jugador = Jugador("Daniel Medina", "12345678X", date(1990, 5, 14), 1010)
+print(jugador)
